ec387f9b3520b6a0101a1031844f1b51b9fb84b3
shiroshana <shiroshana@gmail.com>
2011-07-11 14:52:03 -0400

    Addes a Pages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Twitterapp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
+  get "pages/s"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
